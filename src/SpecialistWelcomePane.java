cc257eeec6f7d79d12b96d5b6bde39758ec524a7
Adrianna Garcia <garciaadrianna13@Gmail.com>
2021-11-05 17:52:34 -0700

    commit

--- src/SpecialistWelcomePane.java
+++ src/SpecialistWelcomePane.java
@@ -1,9 +1,11 @@
+import javafx.collections.FXCollections;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.Pane;
+import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
 import javafx.scene.paint.Color;
 import java.io.FileNotFoundException;
 import javafx.scene.layout.*;
@@ -13,58 +15,56 @@ public class SpecialistWelcomePane {
     private Button viewPatientInfoButton, beginPatientVisitButton, viewAllMessagesButton, logOutButton;
     private Label helloLabelDr, helloLabelNurse, welcomeLabel;
     private ComboBox selectPatient;
-    private Pane background;
     private String specialistName = "";
-    //private Array<PatientNames> namesList; //array of patient names for combo box
+    private ObservableList<String> list = FXCollections.observableArrayList("A","B","C");
 
     public SpecialistWelcomePane() throws FileNotFoundException {
         //LABELS
         helloLabelDr = new Label("Hello, Dr. " + specialistName);
         helloLabelDr.getStyleClass().add("blueLabel");
-        // helloLabelDr.setLayoutX();
-        // helloLabelDr.setLayoutY();
+        helloLabelDr.setLayoutX(0);
+        helloLabelDr.setLayoutY(0);
 
         helloLabelNurse = new Label("Hello, " + specialistName + "R.N.");
         helloLabelNurse.getStyleClass().add("blueLabel");
-        // helloLabelNurse.setLayoutX();
-        // helloLabelNurse.setLayoutY();
+        helloLabelNurse.setLayoutX(0);
+        helloLabelNurse.setLayoutY(0);
 
         welcomeLabel = new Label("Welcome to ConnectMD");
         welcomeLabel.getStyleClass().add("blueLabel");
-        //welcomeLabel.setLayoutX();
-        //welcomeLabel.setLayoutY();
+        welcomeLabel.setLayoutX(0);
+        welcomeLabel.setLayoutY(0);
 
         //BUTTONS
         logOutButton = new Button("Log Out");
         logOutButton.getStyleClass().add("blueLogOutButton");
-        //logOutButton.setLayoutX();
-        //logOutButton.setLayoutY();
+        logOutButton.setLayoutX(0);
+        logOutButton.setLayoutY(0);
 
         viewPatientInfoButton = new Button("View Patient Information");
         viewPatientInfoButton.getStyleClass().add("bluePatientInfoButton");
-        //viewPatientInfoButton.setLayoutX();
-        //viewPatientInfoButton.setLayoutY();
+        viewPatientInfoButton.setLayoutX(0);
+        viewPatientInfoButton.setLayoutY(0);
 
         beginPatientVisitButton = new Button("Begin Patient Visit");
         beginPatientVisitButton.getStyleClass().add("blueBeginVisitButton");
-        //beginPatientVisitButton.setLayoutX();
-        //beginPatientVisitButton.setLayoutY();
+        beginPatientVisitButton.setLayoutX(0);
+        beginPatientVisitButton.setLayoutY(0);
 
         viewAllMessagesButton = new Button("View All Messages");
         viewAllMessagesButton.getStyleClass().add("blueViewMessageButton");
-        //viewAllMessagesButton.setLayoutX();
-        //viewAllMessagesButton.setLayoutY();
+        viewAllMessagesButton.setLayoutX(0);
+        viewAllMessagesButton.setLayoutY(0);
 
         //COMBO BOX
-       // namesList = new Array<PatientNames>();
-       // selectPatient = new ComboBox(namesList);
+        selectPatient = new ComboBox(list);
         selectPatient.setValue("Select Patient");
-        //selectPatient.setLayoutX();
-        //selectPatient.setLayoutY();
+        selectPatient.setLayoutX(0);
+        selectPatient.setLayoutY(0);
 
         //Set the background and children
-        //this.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
-        //this.getChildren().addAll(viewAllMessagesButton,viewPatientInfoButton,beginPatientVisitButton,logOutButton,helloLabelDr,helloLabelNurse,selectPatient);
+        this.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
+        this.getChildren().addAll(viewAllMessagesButton,viewPatientInfoButton,beginPatientVisitButton,logOutButton,helloLabelDr,helloLabelNurse,selectPatient);
 
         //Action Events
         viewAllMessagesButton.setOnAction(event -> SceneController.changeSceneTest());

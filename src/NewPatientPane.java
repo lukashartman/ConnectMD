507164a60ff6aaacea8eb1f8126f2c08038f7e3f
bsmalkin <77949512+bsmalkin@users.noreply.github.com>
2021-11-05 22:14:53 -0700

    commit

--- src/NewPatientPane.java
+++ src/NewPatientPane.java
@@ -19,27 +19,27 @@ public class PatientRegistrationPane extends StackPane
     private BorderPane border; 
     private TextField firstName, lastName, email, password, passwordConfirm; 
     private Button confirm, cancel; 
-    
-    
+     
     
     public void newPatientPane()
     {
      
         fieldsVBox = new VBox(15);
-        left = new VBox(); 
-        right = new VBox();
-        buttonBox = new HBox(); 
-        border = new BorderPane();
+        left = new VBox(); // left side VBox 
+        right = new VBox(); // right side VBox
+        buttonBox = new HBox();  // HBox that contains buttons
+        border = new BorderPane(); 
         
-        firstName = new TextField();
-        lastName = new TextField();
-        email = new TextField();
-        password = new TextField();
-        passwordConfirm = new TextField();
-        confirm = new Button();
-        cancel = new Button();
+        firstName = new TextField(); // first name text field 
+        lastName = new TextField(); // last name text field 
+        email = new TextField(); // email text field 
+        password = new TextField(); // password text field 
+        passwordConfirm = new TextField(); // confirm password text field 
+        confirm = new Button(); // confirm patient registration button 
+        cancel = new Button(); // cancel patient registration button 
         
-        firstName.setText("First Name");
+        // set default text of all text fields 
+        firstName.setText("First Name"); 
         lastName.setText("Last Name");
         email.setText("Email");
         password.setText("Password");
@@ -47,20 +47,23 @@ public class PatientRegistrationPane extends StackPane
         confirm.setText("Confirm");
         cancel.setText("Cancel");
         
+        // set ailgnment and widths 
         fieldsVBox.setAlignment(Pos.CENTER);
         fieldsVBox.setPrefWidth(500);
         buttonBox.setAlignment(Pos.CENTER);
         left.setPrefWidth(100);
         right.setPrefWidth(100);
         
+        // add nodes to button HBox and field VBox
         buttonBox.getChildren().addAll(confirm, cancel);
         fieldsVBox.getChildren().addAll(firstName, lastName, email, password, passwordConfirm, buttonBox); 
         
+        //set contents of Border Pane
         border.setCenter(fieldsVBox);
         border.setLeft(left);
         border.setRight(right);
         
-        this.getChildren().add(border);
+        this.getChildren().add(border); // set contents of New Patient Pane
     }
     
 }

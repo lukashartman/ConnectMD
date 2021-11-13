1226ffe3e452738f7242f55562d315b8dfcd6f10
njwilmot <79833856+njwilmot@users.noreply.github.com>
2021-11-13 03:21:59 -0700

    commit

--- src/Patient.java
+++ src/Patient.java
@@ -1,2 +1,12 @@
-public class Patient {
-}
+import java.util.Vector;
+
+//Patient class which stores patient data from PatientNode
+
+public class Patient
+{
+    private Vector<PatientNode> patientData;
+
+    public void retrievePatient()
+    {
+        patientData = new Vector<PatientNode>();
+    }
